caabe38a2ca0c8f5ad8d4c85989ac4e6715345e4
Chiranth C <chiranthchandrashekar123@gmail.com>
2023-08-10 22:11:01 +0530

    adding git ignore and pytest files

---
+++ library/test_pytest.py
@@ -0,0 +1,9 @@
+# pytest --verbose test_pytest.py --html=../reports/result.html
+def sample_function(param):
+    return param + 1
+
+def test_success():
+    assert sample_function(2) == 3
+
+def test_failure():
+    assert sample_function(1) == 3
